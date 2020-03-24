4578d6e8eaa59bfe417c38a6c391861df6b63fd2
SKITLZ <gskitlz@gmail.com>
2020-03-24 22:56:17 +0300

    Chemical X

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,12 @@ import { join }  from 'path';
 import { NestFactory } from '@nestjs/core';
 import { ApplicationModule } from './app.module';
 
-const SERVER_PORT = process.env.PORT || 3000;
+const NEST_PORT = process.env.PORT || 3000;
 const CLIENT_FILES = join(__dirname, '..', '..', 'client', 'dist');
 
 async function bootstrap() {
-	const app = await NestFactory.create(ApplicationModule);
-	app.use(express.static(CLIENT_FILES));
-	await app.listen(SERVER_PORT);
+ const app = await NestFactory.create(ApplicationModule);
+ app.use(express.static(CLIENT_FILES));
+ await app.listen(NEST_PORT);
 }
 bootstrap();
